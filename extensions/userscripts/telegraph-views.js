48758648b44bc3b1ab1ab12e9c6308c80d798c14
InfoZebra <79378855+InfoZebra@users.noreply.github.com>
2021-02-21 19:37:25 +0200

    Update and rename telegra.ph to telegraph-views.js

---
+++ extensions/userscripts/telegraph-views.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         Telegraph Views
+// @namespace    https://open.lbry.com/@InfoZebra:1?r=7YcRBCZ7iR8tbx7H4uu3TJqP1Gc3qoEK
+// @version      0.2
+// @description  Show amount of views on Telegraph pages
+// @author       gcontent
+// @match        https://*.telegra.ph/*
+// @grant        none
+// ==/UserScript==
+
+
+
+(function() {
+    'use strict';
+    setInterval(setDownloader, 1000);
+    function setDownloader() {
+        let link = document.getElementById('downloader');
+        let LinkAPI = "https://api.telegra.ph/getViews" + window.location.pathname;
+
+        function Get(LinkAPI){
+        var Httpreq = new XMLHttpRequest(); // a new request
+        Httpreq.open("GET",LinkAPI,false);
+        Httpreq.send(null);
+        return Httpreq.responseText;
+        }
+
+        var json_obj = JSON.parse(Get(LinkAPI));
+        console.log("Количество просмотров: " + json_obj.result.views);
+
+        if (!link) {
+            var innerText = LinkAPI
+            link = document.createElement('a');
+            link.innerText = " • " + json_obj.result.views;
+            link.setAttribute('target', '_blank');
+            link.setAttribute('id', 'downloader');
+            document.getElementsByTagName('address')[0].appendChild(link)
+        }
+        let hrefChannel = "https://open.lbry.com/@InfoZebra:1?r=7YcRBCZ7iR8tbx7H4uu3TJqP1Gc3qoEK";
+
+        link.setAttribute('href', hrefChannel);
+    }
+})();
